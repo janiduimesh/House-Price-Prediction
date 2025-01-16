fbaf0891a3866800d616c3ca5434ba18eeadd2a5
janiduimesh <janiduimesh@gmail.com>
2025-01-16 19:55:37 +0530

    full server and util

--- server/util.py
+++ server/util.py
@@ -1,8 +1,25 @@
 import json
 import pickle
+import numpy as np
+
 __locations = None
 __data_columns = None
 __model = None
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index=-1
+    
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >=0:
+        x[loc_index] = 1
+            
+    return round(__model.predict([x])[0], 2)
 
 def get_location_names():
     return __locations
@@ -11,6 +28,7 @@ def load_saved_artifacts():
     print("Loading saved artifacts")
     global __data_columns
     global __locations
+    global __model
     
     with open("./artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
@@ -22,4 +40,5 @@ def load_saved_artifacts():
     
 if __name__ == '__main__':
     load_saved_artifacts()
-    print(get_location_names())    
+    # print(get_location_names())    
+    print(get_estimated_price('kalhalli',1000,2,2))
